c0413225728329b83aa09b4d84c2a252dfdcb2a2
gioele <gioelefriggia@hotmail.com>
2026-07-17 10:14:15 +0100

    mod supplierInfo

--- src/data/supplierInfo.js
+++ src/data/supplierInfo.js
@@ -5,7 +5,7 @@ export const supplierInfo = {
       "FIR001 – 49mm Round Label – 10,000 per Case",
       "FIR002 – 64mm Round Label – 6,000 per Case",
     ],
-    email: "info@cafebrands.ie",
+
     website: "https://www.cafebrands.ie/software/",
   },
   packagingSupplier: {
